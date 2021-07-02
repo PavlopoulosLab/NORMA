b07b37124da50a45b90921a61478352fb9ccac72
PavlopoulosLab <61477518+PavlopoulosLab@users.noreply.github.com>
2021-07-02 13:44:02 +0300

    Add files via upload

--- ui.R
+++ ui.R
@@ -1220,10 +1220,10 @@ The Annotation Tab consists of three sub-tabs. These are the: (i) Convex Hull, (
       helpText("Available at: https://github.com/PavlopoulosLab/NORMA"),
       br(),
       strong("Publications:"),
-      helpText(
-        "NORMA has been submitted for publication.
-            Information will be provided once the paper is online"
-      )
+	  br(),
+      	tags$a(href="https://pubmed.ncbi.nlm.nih.gov/34171457/", "NORMA: The Network Makeup Artist - A Web Tool for Network Annotation Visualization."),
+            helpText(" Genomics Proteomics Bioinformatics. 2021 Jun 22:S1672-0229(21)00130-3. doi: 10.1016/j.gpb.2021.02.005. Epub ahead of print. PMID: 34171457."),
+           
       
     )
   )
